b50938b666ca4f07e4a89cf9458b2d96211ea66f
Matthew Gregory Browne <odbgza@gmail.com>
2013-04-24 12:31:41 +1000

    Fixed aysnchronous json getting bug, modularised code
    * Flow of execution is sequential for retrieving JSON files
    * User data and points are now in a single JSON object for each
    datapoint
    * Modularised code into multiple functions

--- scripts/visualisation.js
+++ scripts/visualisation.js
@@ -2,55 +2,93 @@
 	- plots a scatterplot of json defined coordinates
 */
 var points;
-var user_data;
+var userdata;
+var dataset = [];
 
 var svg = d3.select("#widget")
 	.append("svg")
 	.attr("width", "100%")
 	.attr("height", "100%");
 
-// Retrieve user data from user_data.json
+// Retrieve user data from userdata.json
 d3.json("json/user_data.json", function(json) {
-	user_data = json;
+	userdata = json;
+	getPoints();
 });
 
 // Retrieve points data from points.json
-d3.json("json/points.json", function(json) {
-	points = json;
+
+function getPoints() {
+	d3.json("json/points.json", function(json) {
+		points = json;
+		mergeJSON();
+	});
+}
+// Merge points and user data
+
+function mergeJSON() {
 
 	// Add the user data to the points
-	for (var i = 0; i < json.length; i++) {
-		points[i].push({ //d[2]
-			colour: user_data[i].colour,
-			cred: user_data[i].cred,
-			id: user_data[i].id,
-			link: user_data[i].link,
-			primary: user_data.primary,
-			username: user_data[i].username
+	for (var i = 0; i < points.length; i++) {
+		dataset.push({
+			x: points[i][0],
+			y: points[i][1],
+			colour: userdata[i].colour,
+			cred: userdata[i].cred,
+			id: userdata[i].id,
+			link: userdata[i].link,
+			primary: userdata[i].primary,
+			username: userdata[i].username
 		});
 
-		// Scale points to be spaced out better
-		for (var j = 0; j < 2; j++) {
-			points[i][j] = (points[i][j] + 5) * 50;
-		}
+	}
+	console.log(dataset);
+
+	// Add the user data to the points
+	for (var i = 0; i < points.length; i++) {
+		points[i].push({ //d[2]
+			colour: userdata[i].colour,
+			cred: userdata[i].cred,
+			id: userdata[i].id,
+			link: userdata[i].link,
+			primary: userdata[i].primary,
+			username: userdata[i].username
+		});
+
+
 	}
 
+	draw();
 	// DEBUG
-	console.log(points);
+	// console.log(points);
+}
+
+function scaleCoordinates() {
+	// Scale points to be spaced out better
+	for (var i = 0; i < dataset.length; i++) {
+		dataset[i].x = (dataset[i].x + 5) * 50
+		dataset[i].y = (dataset[i].y + 5) * 50
+		// points[i][j] = (points[i][j] + 5) * 50;
+	}
+}
+
+function draw() {
+
+	scaleCoordinates();
 
 	// Configure circles
 	svg.selectAll("circle")
-		.data(points)
+		.data(dataset)
 		.enter()
 		.append("circle")
 		.on("mouseover", function(d) {
-		console.log(d[2].username);
+		console.log(d.username);
 	})
 		.attr("cx", function(d) {
-		return d[0];
+		return d.x;
 	})
 		.attr("cy", function(d) {
-		return d[1];
+		return d.y;
 	})
 		.style("stroke", "#000")
 		.transition()
@@ -58,27 +96,32 @@ d3.json("json/points.json", function(json) {
 		return 20;
 	})
 		.style("fill", function(d) {
-		if (d[2].colour == "red") return "darkred";
-		else if (d[2].colour == "blue") return "darkblue";
-		else if (d[2].colour == "green") return "darkgreen";
-		else return d[2].colour;
+		if (d.colour == "red") return "darkred";
+		else if (d.colour == "blue") return "darkblue";
+		else if (d.colour == "green") return "darkgreen";
+		else return d.colour;
 	});
 
+	setLabels();
+
+}
+
+function setLabels() {
 	// Configure labels
-	// svg.selectAll("text")
-	// 	.data(points)
-	// 	.enter()
-	// 	.append("text")
-	// 	.attr("dx", function(d) {
-	// 	return d[0];
-	// })
-	// 	.attr("dy", function(d) {
-	// 	return d[1] - 25;
-	// })
-	// 	.attr("font-family", "sans-serif")
-	// 	.attr("font-size", "13px")
-	// 	.style("text-anchor", "middle")
-	// 	.text(function(d) {
-	// 	return d[2].username;
-	// });
-});
+	svg.selectAll("text")
+		.data(dataset)
+		.enter()
+		.append("text")
+		.attr("dx", function(d) {
+		return d.x;
+	})
+		.attr("dy", function(d) {
+		return d.y - 25;
+	})
+		.attr("font-family", "sans-serif")
+		.attr("font-size", "13px")
+		.style("text-anchor", "middle")
+		.text(function(d) {
+		return d.username;
+	});
+}
