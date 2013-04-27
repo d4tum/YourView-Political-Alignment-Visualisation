b30281cb4508abd2584b0819a5c2a7c5e6a82c7d
Matthew Gregory Browne <odbgza@gmail.com>
2013-04-27 14:29:14 +1000

    Added toggle entity logic.
    * Re-wrote much of the code to fix the trouble with removing an entity
    (enter-update-exit paradigm)
    * Added buttons in the entity tab to show or hide entities
    * Regression - labels will need to be re-implemented

--- scripts/visualisation.js
+++ scripts/visualisation.js
@@ -1,31 +1,9 @@
-/*JavaScript file that provides the functionality of the yourview.org.au political alignmenet visualisation:
-This is:
-	- layout of scatterplot (output) and controls (input)
-	- plots a scatterplot of json defined coordinates when the html page is first loaded
-	- re-plots the scatterplot with fresh points when a slider value is changed
-
-TODO:
-	- Move from emulating the API return files wtih lical dummy files to actual API calls
-	- Wire up sliders to write to file/POST etc. to send to server the tag weights
-	- Implement Entities tab with buttons
-	- Implement show/hide of plot point of the entity button clicked
-	- Overall style tidy, prettify, tweaking
-*/
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~d3 stuff~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-var DEBUG = true;
-// For bug where text labels are only half their supposed x value
-// See http://stackoverflow.com/questions/7000190/detect-all-firefox-versions-in-js
-// for this solution.
-var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-
-var points;
-var userdata;
-var data = [];
 var w = 600
 var h = 600
-var entities;
+var data = []; // datapoints - an array of json objects
+var points; // points of the points.json file
+var userdata; // user data from the user_data.json file
+var visibility = []; // boolean array for keeping a record of entities shown
 
 var svg = d3.select("#scatterplot")
 	.append("svg")
@@ -35,64 +13,62 @@ var svg = d3.select("#scatterplot")
 // Retrieve user data from userdata.json
 d3.json("json/user_data.json", function(json) {
 	userdata = json;
-	retrievePoints();
+	retrievePoints(userdata);
 });
 
-// Retrieve points data from points.json
-
-function retrievePoints() {
+function retrievePoints(userdata) {
 	d3.json("json/points.json", function(json) {
 		points = json;
-		data = mergeJSON(data);
-		plot();
+		initEnabled();
+		data = createData();
+		plotData();
 	});
 }
 
-// Merge the points and user data from the json files
-// into a single object per datum/plot point/political entity
+function initEnabled() {
+	for (var i = 0; i < points.length; i++) {
+		visibility.push({
+			username: userdata[i].username,
+			enabled: true
+		});
+	}
+	console.log(visibility);
+}
 
-function mergeJSON(dataset) {
-
+function createData() {
+	var dataset = [];
 	// Add the user data to the points
 	for (var i = 0; i < points.length; i++) {
-		dataset.push({
-			x: points[i][0],
-			y: points[i][1],
-			colour: userdata[i].colour,
-			cred: userdata[i].cred,
-			id: userdata[i].id,
-			link: userdata[i].link,
-			primary: userdata[i].primary,
-			username: userdata[i].username
-		});
-
+		if (visibility[i].enabled) {
+			dataset.push({
+				x: points[i][0],
+				y: points[i][1],
+				colour: userdata[i].colour,
+				cred: userdata[i].cred,
+				id: userdata[i].id,
+				link: userdata[i].link,
+				primary: userdata[i].primary,
+				username: userdata[i].username
+			});
+		}
 	}
-
-	if (DEBUG) console.log(data);
 
 	return dataset;
 }
 
-// Scale scatter plot points to be spaced out better
-// TODO: Generalise this to calculate the scaling factor
-// based on range of values.
-
-function scale(set) {
-	for (var i = 0; i < set.length; i++) {
-		set[i].x = (set[i].x + 5) * 50
-		set[i].y = (set[i].y + 5) * 50
+function scale(dataset) {
+	for (var i = 0; i < dataset.length; i++) {
+		dataset[i].x = (dataset[i].x + 5) * 50
+		dataset[i].y = (dataset[i].y + 5) * 50
 		// points[i][j] = (points[i][j] + 5) * 50;
 	}
-	return set
+	return dataset
 }
 
-// For the time being, while the cross site access gets sorted,
-// We will use dummy json files to emulate calls to the API
-// This is the purpose of the function below.
 var previousIndex;
 
 function chooseRandDummyFile() {
-	var pathArray = [];
+	var array = [];
 
 	path1 = "json/dummy_points1.json";
 	path2 = "json/dummy_points2.json";
@@ -100,11 +76,11 @@ function chooseRandDummyFile() {
 	path4 = "json/dummy_points4.json";
 	path5 = "json/dummy_points5.json";
 
-	pathArray.push(path1);
-	pathArray.push(path2);
-	pathArray.push(path3);
-	pathArray.push(path4);
-	pathArray.push(path5);
+	array.push(path1);
+	array.push(path2);
+	array.push(path3);
+	array.push(path4);
+	array.push(path5);
 
 	// Make sure we choose an index different to the last one.
 	while (true) {
@@ -116,29 +92,46 @@ function chooseRandDummyFile() {
 	// Adjust index for zero base
 	index -= 1;
 
-	if (DEBUG) console.log(index);
-
-	return pathArray[index];
-
+	return array[index];
 }
 
-// Called when a slider is changed (see line: 13).
-// ATM the JSON API is not used and is replaced with a few random JSON files of points.
-// Also implements the transition between the old and new points in the scatter plot.
+function plotData() {
+	svg.selectAll("g")
+		.data(scale(data)).enter()
+		.append("g")
+		.on("mouseover", function(d) {
+		var sel = d3.select(this);
+		sel.moveToFront();
+		console.log(d.username);
+	})
+		.append("circle")
+		.attr("cx", function(d) {
+		return d.x;
+	})
+		.attr("cy", function(d) {
+		return d.y;
+	})
+		.style("fill", function(d) {
+		return d.colour;
+	})
+		.transition()
+		.duration(700)
+		.attr("r", function(d) {
+		return 15;
+	});
+}
 
-function update() {
+function updatePlot() {
 	d3.json(chooseRandDummyFile(), function(json) {
-		updatedData = new Array();
 		points = json;
-		updatedData = mergeJSON(updatedData);
 
-		if (DEBUG) console.log(updatedData);
+		data = createData();
 
-		// enter() and append() are omitted as we are using transision()
+		// enter() and append() are omitted for a transision()
 		svg.selectAll("circle")
-			.data(scale(updatedData))
+			.data(scale(data))
 			.transition()
-			.duration(1500)
+			.duration(1100)
 			.attr("cx", function(d) {
 			return d.x;
 		})
@@ -151,89 +144,59 @@ function update() {
 			.style("fill", function(d) {
 			return d3.rgb(d.colour);
 		});
-
-		// Move the labels too
-		updateLabels();
-
-	});
-
-}
-
-// Called when the visualisation is first loaded.
-// Binds the data to the circle svg elements and sets the attributes for a datum.
-
-function plot() {
-	entities = svg.selectAll("g")
-		.data(scale(data))
-		.enter()
-		.append("g")
-		.on("mouseover", function(d) {
-		var sel = d3.select(this);
-		sel.moveToFront();
-		console.log(d.username);
-	});
-
-	entities.append("circle")
-		.attr("cx", function(d) {
-		return d.x;
-	})
-		.attr("cy", function(d) {
-		return d.y;
-	})
-		.style("fill", function(d) {
-		return d.colour;
-	})
-		.transition()
-		.duration(800)
-		.attr("r", function(d) {
-		return 15;
-	});
-
-	entities.append("text")
-		.attr("dx", function(d) {
-		if (is_firefox) return d.x * 2;
-		else return d.x;
-	})
-		.attr("dy", function(d) {
-		return d.y + 40;
-	})
-		.attr("font-family", "sans-serif")
-		.attr("font-size", "13px")
-		.style("text-anchor", "middle")
-		.text(function(d) {
-		return d.username;
 	});
 }
 
-// Animates the labels
+function toggleEntity(enable) {
 
-function updateLabels() {
-	svg.selectAll("text")
-		.data(updatedData)
-		.transition()
-		.duration(1500)
-		.attr("dx", function(d) {
-		if (is_firefox) return d.x * 2;
-		return d.x;
-	})
-		.attr("dy", function(d) {
-		return d.y + 40;
-	})
-		.attr("font-family", "sans-serif")
-		.attr("font-size", "13px")
-		.style("text-anchor", "middle")
-		.text(function(d) {
-		return d.username;
-	});
+	data = createData();
+
+	if (!enable) {
+		svg.selectAll('circle')
+			.data(scale(data), function(d) {
+			return (d.username);
+		})
+			.exit().transition()
+			.attr("r", 0)
+			.remove();
+	} else {
+		svg.selectAll("circle")
+			.data(scale(data), function(d) {
+			return (d.username);
+		}).enter()
+			.append("circle")
+			.on("mouseover", function(d) {
+			var sel = d3.select(this);
+			sel.moveToFront();
+			console.log(d.username);
+		})
+			.attr("cx", function(d) {
+			return d.x;
+		})
+			.attr("cy", function(d) {
+			return d.y;
+		})
+			.style("fill", function(d) {
+			return d.colour;
+		})
+			.transition()
+			.duration(700)
+			.attr("r", function(d) {
+			return 15;
+		});
+	}
+
+
+	console.log(data);
+
 }
 
-// Prototype of moveToFront to move an obscured plot point to the front.
-// See mouse over on line: 183
 d3.selection.prototype.moveToFront = function() {
 	return this.each(function() {
 		this.parentNode.appendChild(this);
 	});
 };
+
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~jQuery stuff~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Create tabs
@@ -246,26 +209,101 @@ $(function() {
 	$("#slider1").slider().slider("option", "min", -1).slider({
 		max: 1
 	}).on("slidestop", function(event, ui) {
-		update();
+		updatePlot();
 	});
 	$("#slider2").slider().slider("option", "min", -1).slider({
 		max: 1
 	}).on("slidestop", function(event, ui) {
-		update();
+		updatePlot();
 	});
 	$("#slider3").slider().slider("option", "min", -1).slider({
 		max: 1
 	}).on("slidestop", function(event, ui) {
-		update();
+		updatePlot();
 	});
 	$("#slider4").slider().slider("option", "min", -1).slider({
 		max: 1
 	}).on("slidestop", function(event, ui) {
-		update();
+		updatePlot();
 	});
 	$("#slider5").slider().slider("option", "min", -1).slider({
 		max: 1
 	}).on("slidestop", function(event, ui) {
-		update();
+		updatePlot();
+	});
+});
+
+// 1. find the username of the circle clicked
+// 2. toggle it's enabled state
+// 3. create a new data array bad bind it to circle
+// 4. call exit().remove()
+// see - http://mbostock.github.io/d3/tutorial/circle.html
+$(function() {
+	$("#button1").click(function() {
+		for (var i = 0; i < visibility.length; i++) {
+			if (visibility[i].username == 'Liberals') {
+				console.log(visibility[i].enabled);
+				visibility[i].enabled = !visibility[i].enabled;
+				toggleEntity(visibility[i].enabled);
+			}
+		}
+
+	});
+});
+
+$(function() {
+	$("#button2").click(function() {
+
+		for (var i = 0; i < visibility.length; i++) {
+			if (visibility[i].username == 'Labor') {
+				console.log(visibility[i].enabled);
+				visibility[i].enabled = !visibility[i].enabled;
+				console.log(visibility[i].enabled);
+				toggleEntity(visibility[i].enabled);
+			}
+		}
+
+	});
+});
+
+$(function() {
+	$("#button3").click(function() {
+
+		for (var i = 0; i < visibility.length; i++) {
+			if (visibility[i].username == 'Greens') {
+				console.log(visibility[i].enabled);
+				visibility[i].enabled = !visibility[i].enabled;
+				toggleEntity(visibility[i].enabled);
+			}
+		}
+
+	});
+});
+
+$(function() {
+	$("#button4").click(function() {
+
+		for (var i = 0; i < visibility.length; i++) {
+			if (visibility[i].username == 'Nationals') {
+				console.log(visibility[i].enabled);
+				visibility[i].enabled = !visibility[i].enabled;
+				toggleEntity(visibility[i].enabled);
+			}
+		}
+
+	});
+});
+
+$(function() {
+	$("#button5").click(function() {
+
+		for (var i = 0; i < visibility.length; i++) {
+			if (visibility[i].username == 'timvangelder') {
+				console.log(visibility[i].enabled);
+				visibility[i].enabled = !visibility[i].enabled;
+				toggleEntity(visibility[i].enabled);
+			}
+		}
+
 	});
 });
